df249653bc8c4244e8d4de6637a294b36a66bea6
Joshua Behrens <behrens@heptacom.de>
2021-06-29 21:46:41 +0200

    Add missing static folder in path
    HCCLOUD-8

--- resources/views/integration/shopware-6/wizard.blade.php
+++ resources/views/integration/shopware-6/wizard.blade.php
@@ -23,10 +23,10 @@
     </div>
 </div>
 
-@if (file_exists(public_path('storage/shopware-admin/v'.$swVersion.'/css/app.css')))
-    <link rel="stylesheet" href="/storage/shopware-admin/v{{ $swVersion }}/css/app.css">
-@elseif (file_exists(public_path('storage/shopware-admin/v6.4.0.0/css/app.css')))
-    <link rel="stylesheet" href="/storage/shopware-admin/v6.4.0.0/css/app.css">
+@if (file_exists(public_path('storage/shopware-admin/v'.$swVersion.'/static/css/app.css')))
+    <link rel="stylesheet" href="/storage/shopware-admin/v{{ $swVersion }}/static/css/app.css">
+@elseif (file_exists(public_path('storage/shopware-admin/v6.4.0.0/static/css/app.css')))
+    <link rel="stylesheet" href="/storage/shopware-admin/v6.4.0.0/static/css/app.css">
 @else
     <link rel="stylesheet" href="{{ request('shop-url') }}/bundles/administration/static/css/app.css?{{ request('timestamp') }}">
 @endif
