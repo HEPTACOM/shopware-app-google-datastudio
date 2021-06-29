07fa1b709e9c7a8a3c7e30c37dfd2eefe0677cd6
Joshua Behrens <behrens@heptacom.de>
2021-06-29 22:07:37 +0200

    Transform wizard html fragment into an html page
    HCCLOUD-8

--- resources/views/integration/shopware-6/wizard.blade.php
+++ resources/views/integration/shopware-6/wizard.blade.php
@@ -1,95 +1,105 @@
-<div class="sw-page__content">
-    <div class="sw-page__main-content">
-        <div class="sw-page__main-content-inner">
-            <div class="sw-card-view">
-                <div class="sw-card-view__content">
-                    <div class="sw-card">
-                        <div class="sw-card__title">
-                            {{ trans('shopware-6/wizard.header.title') }}
-                        </div>
-                        <div class="sw-card__subtitle">
-                            {{ trans('shopware-6/wizard.header.subtitle') }}
-                        </div>
-                        <div class="sw-card__content wizard wizard-page-selected--intro">
-                            @include('integration.shopware-6.wizard.page-intro')
-                            @include('integration.shopware-6.wizard.page-data-config')
-                            @include('integration.shopware-6.wizard.page-setup')
-                            @include('integration.shopware-6.wizard.page-finish')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Shopware 6 Wizard</title>
+
+        @if (file_exists(public_path('storage/shopware-admin/v'.$swVersion.'/static/css/app.css')))
+            <link rel="stylesheet" href="/storage/shopware-admin/v{{ $swVersion }}/static/css/app.css">
+        @else
+            <link rel="stylesheet" href="/storage/shopware-admin/v6.4.0.0/static/css/app.css">
+        @endif
+
+        <style>
+            .wizard-page {
+                display: none;
+            }
+
+            .wizard-page p {
+                margin-bottom: 16px;
+                white-space: pre-wrap;
+            }
+
+            footer {
+                display: flex;
+                justify-content: space-between;
+                margin-top: 30px;
+            }
+
+            .wizard-page.wizard-page--data-config ul {
+                padding-left: 20px;
+                margin: 20px 0;
+            }
+
+            .sw-card__content.wizard-page-selected--intro .wizard-page.wizard-page--intro {
+                display: block;
+            }
+
+            .sw-card__content.wizard-page-selected--data-config .wizard-page.wizard-page--data-config {
+                display: block;
+            }
+
+            .sw-card__content.wizard-page-selected--setup .wizard-page.wizard-page--setup {
+                display: block;
+            }
+
+            .sw-card__content.wizard-page-selected--finish .wizard-page.wizard-page--finish {
+                display: block;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="sw-page__content">
+            <div class="sw-page__main-content">
+                <div class="sw-page__main-content-inner">
+                    <div class="sw-card-view">
+                        <div class="sw-card-view__content">
+                            <div class="sw-card">
+                                <div class="sw-card__title">
+                                    {{ trans('shopware-6/wizard.header.title') }}
+                                </div>
+                                <div class="sw-card__subtitle">
+                                    {{ trans('shopware-6/wizard.header.subtitle') }}
+                                </div>
+                                <div class="sw-card__content wizard wizard-page-selected--intro">
+                                    @include('integration.shopware-6.wizard.page-intro')
+                                    @include('integration.shopware-6.wizard.page-data-config')
+                                    @include('integration.shopware-6.wizard.page-setup')
+                                    @include('integration.shopware-6.wizard.page-finish')
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
 
-@if (file_exists(public_path('storage/shopware-admin/v'.$swVersion.'/static/css/app.css')))
-    <link rel="stylesheet" href="/storage/shopware-admin/v{{ $swVersion }}/static/css/app.css">
-@else
-    <link rel="stylesheet" href="/storage/shopware-admin/v6.4.0.0/static/css/app.css">
-@endif
+        <script type="application/javascript">
+            function wizardGoTo(pageId) {
+                /** @var wizard {HTMLDivElement} */
+                for (let wizard of document.querySelectorAll('.wizard')) {
+                    let classesToRemote = [];
 
-<style>
-    .wizard-page {
-        display: none;
-    }
+                    for (let className of wizard.classList.values()) {
+                        if (className.match(/^wizard-page-selected--/)) {
+                            classesToRemote.push(className);
+                        }
+                    }
 
-    .wizard-page p {
-        margin-bottom: 16px;
-        white-space: pre-wrap;
-    }
+                    for (let className of classesToRemote) {
+                        wizard.classList.remove(className);
+                    }
 
-    footer {
-        display: flex;
-        justify-content: space-between;
-        margin-top: 30px;
-    }
-
-    .wizard-page.wizard-page--data-config ul {
-        padding-left: 20px;
-        margin: 20px 0;
-    }
-
-    .sw-card__content.wizard-page-selected--intro .wizard-page.wizard-page--intro {
-        display: block;
-    }
-
-    .sw-card__content.wizard-page-selected--data-config .wizard-page.wizard-page--data-config {
-        display: block;
-    }
-
-    .sw-card__content.wizard-page-selected--setup .wizard-page.wizard-page--setup {
-        display: block;
-    }
-
-    .sw-card__content.wizard-page-selected--finish .wizard-page.wizard-page--finish {
-        display: block;
-    }
-</style>
-
-<script type="application/javascript">
-    function wizardGoTo(pageId) {
-        /** @var wizard {HTMLDivElement} */
-        for (let wizard of document.querySelectorAll('.wizard')) {
-            let classesToRemote = [];
-
-            for (let className of wizard.classList.values()) {
-                if (className.match(/^wizard-page-selected--/)) {
-                    classesToRemote.push(className);
+                    wizard.classList.add('wizard-page-selected--' + pageId);
                 }
             }
 
-            for (let className of classesToRemote) {
-                wizard.classList.remove(className);
+            function sendReadyState() {
+                window.parent.postMessage('sw-app-loaded', '*');
             }
 
-            wizard.classList.add('wizard-page-selected--' + pageId);
-        }
-    }
-
-    function sendReadyState() {
-        window.parent.postMessage('sw-app-loaded', '*');
-    }
-
-    sendReadyState();
-</script>
+            sendReadyState();
+        </script>
+    </body>
+</html>
