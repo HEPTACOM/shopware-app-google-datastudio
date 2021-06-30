c53319a536e986b641aeed10a3ddcf1be4410b03
Julian Krzefski <krzefski@heptacom.de>
2021-06-30 18:30:57 +0200

    HCCLOUD-8: Add build command for shopware app

---
+++ app/Console/Commands/Build.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\View;
+
+class Build extends Command
+{
+    protected $signature = 'app:build';
+
+    public function handle()
+    {
+        $prefix = config('heptaconnect-shopware-six.app_name');
+
+        $buildDisk = Storage::build([
+            'driver' => 'local',
+            'root' => storage_path('app/build'),
+        ]);
+
+        $resourcesDisk = Storage::build([
+            'driver' => 'local',
+            'root' => base_path('resources'),
+        ]);
+
+        $buildDisk->deleteDirectory($prefix);
+        $buildDisk->put(
+            $prefix . '/manifest.xml',
+            View::make('integration.shopware-6.manifest-xml')->render()
+        );
+        $buildDisk->put(
+            $prefix . '/icon.png',
+            $resourcesDisk->get('img/shopware-app-logo-heptaconnect.png')
+        );
+    }
+}
